f51d7213c1a5284a060a0bde3c3826f447182f14
Yaswanth Kumar <vickyroyal1995@gmail.com>
2021-11-29 14:28:31 +0530

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 var redis = require('redis');
 var client = redis.createClient({
   port      : '6379',
-  host      : '127.0.0.1',
+  host      : 'jmbaxi-elasticcache-rediscluster.mstk5u.clustercfg.use2.cache.amazonaws.com',
   // password  : 'redispassword',
 });
 
